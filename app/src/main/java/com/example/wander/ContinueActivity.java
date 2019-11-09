235b00093fb56150310f881329c75ca14707280f
Kevin Hong <khong10@jhu.edu>
2019-11-08 20:56:14 -0500

    Added button functionality to ContinueActivity

--- app/src/main/java/com/example/wander/ContinueActivity.java
+++ app/src/main/java/com/example/wander/ContinueActivity.java
@@ -2,7 +2,10 @@ package com.example.wander;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContinueActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class ContinueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continue);
+
+        final Button newGame = findViewById(R.id.new_game);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ContinueActivity.this, MainActivity.class);
+                intent.putExtra("startOver", true);
+                startActivity(intent);
+            }
+        });
+
+        final Button continueGame = findViewById(R.id.continue_game);
+        continueGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ContinueActivity.this, MainActivity.class);
+                intent.putExtra("startOver", false);
+                startActivity(intent);
+            }
+        });
     }
 }
