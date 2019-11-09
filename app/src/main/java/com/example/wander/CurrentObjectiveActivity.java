c39a885015d95b80c80b9e8305f3a3ba8273045b
Noah Frassrand <nfrassr1@jhu.edu>
2019-11-08 20:53:58 -0500

    current objective base

---
+++ app/src/main/java/com/example/wander/CurrentObjectiveActivity.java
@@ -0,0 +1,93 @@
+package com.example.wander;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CurrentObjectiveActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_current_objective, container, false);
+    }
+    // Called at the start of the visible lifetime.
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d ("Content Fragment", "onStart");
+        // Apply any required UI change now that the Fragment is visible.
+    }
+
+    // Called at the start of the active lifetime.
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d ("Content Fragment", "onResume");
+        // Resume any paused UI updates, threads, or processes required
+        // by the Fragment but suspended when it became inactive.
+    }
+
+    // Called at the end of the active lifetime.
+    @Override
+    public void onPause(){
+        Log.d ("Content Fragment", "onPause");
+        // Suspend UI updates, threads, or CPU intensive processes
+        // that don't need to be updated when the Activity isn't
+        // the active foreground activity.
+        // Persist all edits or state changes
+        // as after this call the process is likely to be killed.
+        super.onPause();
+    }
+
+    // Called to save UI state changes at the
+    // end of the active lifecycle.
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        Log.d ("Content Fragment", "onSaveInstanceState");
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate, onCreateView, and
+        // onCreateView if the parent Activity is killed and restarted.
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    // Called at the end of the visible lifetime.
+    @Override
+    public void onStop(){
+        Log.d ("Content Fragment", "onStop");
+        // Suspend remaining UI updates, threads, or processing
+        // that aren't required when the Fragment isn't visible.
+        super.onStop();
+    }
+
+    // Called when the Fragment's View has been detached.
+    @Override
+    public void onDestroyView() {
+        Log.d ("Content Fragment", "onDestroyView");
+        // Clean up resources related to the View.
+        super.onDestroyView();
+    }
+
+    // Called at the end of the full lifetime.
+    @Override
+    public void onDestroy(){
+        Log.d ("Content Fragment", "onDestroy");
+        // Clean up any resources including ending threads,
+        // closing database connections etc.
+        super.onDestroy();
+    }
+
+    // Called when the Fragment has been detached from its parent Activity.
+    @Override
+    public void onDetach() {
+        Log.d ("Content Fragment", "onDetach");
+        super.onDetach();
+    }
+}
+
