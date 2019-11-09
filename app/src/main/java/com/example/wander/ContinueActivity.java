5092c58da7d2bc49e7dec65030360c0f1c287229
Kevin Hong <khong10@jhu.edu>
2019-11-08 20:26:24 -0500

    Added layout for ContinueActivity + Roboto font/logo

---
+++ app/src/main/java/com/example/wander/ContinueActivity.java
@@ -0,0 +1,14 @@
+package com.example.wander;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContinueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continue);
+    }
+}
