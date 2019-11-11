c0ea7fd10994674ee2516511a43586b6d1f2f51f
Kevin Hong <khong10@jhu.edu>
2019-11-10 19:04:10 -0500

    Added app bar/nav header templates, map seems to work

--- app/src/main/java/com/example/wander/MainActivity.java
+++ app/src/main/java/com/example/wander/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker at Blue Jay Statue and move the camera
+        LatLng blueJay = new LatLng(39.331089, -76.619615);
+        mMap.addMarker(new MarkerOptions().position(blueJay).title("Marker at Blue Jay Statue"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(blueJay));
     }
 }
--- app/src/main/java/com/example/wander/TabsActivity.java
+++ app/src/main/java/com/example/wander/TabsActivity.java
@@ -1,21 +1,26 @@
 package com.example.wander;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class TabsActivity extends AppCompatActivity {
+public class TabsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tabs);
+        setContentView(R.layout.content_tabs);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +30,23 @@ public class TabsActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_main) {
+            Intent intent = new Intent(TabsActivity.this, TabsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_history) {
+            System.out.println("TEST FIX LATER");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
